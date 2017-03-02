3d90fa56dbdc50b297118e378c11c6c7d41a8465
brunoyui <brunoyui@gmail.com>
2017-03-01 22:04:57 -0300

    Add structure for front-end angular
    Structure generate with angular-cli

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,5 +10,14 @@ app.use(bodyParser.json());
 
 app.use('/transacoes', require('./routes/transacao'));
 app.use('/contas', require('./routes/conta'));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
 
 module.exports = app;
