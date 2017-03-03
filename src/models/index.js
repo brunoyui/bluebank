9f5bca8b61ee62878f8d229fc402707015f85f02
brunoyui <brunoyui@gmail.com>
2017-03-03 10:12:58 -0300

    Bluemix stuff

--- src/models/index.js
+++ src/models/index.js
@@ -6,8 +6,45 @@ var Sequelize = require('sequelize');
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || 'development';
 var config = require('../../config/config.json')[env];
+var cfenv = require('cfenv');
+
+var vcapLocal;
+var connectionString;
+var ca;
+
+try {
+  vcapLocal = require('../vcap-local.json');
+  console.log("Loaded local VCAP", vcapLocal);
+} catch (e) { }
+
+const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {};
+
+const appenv = cfenv.getAppEnv(appEnvOpts);
+
+var services = appenv.services['compose-for-postgresql'];
+
+var sequelize = {};
+
+if (services)
+{
+  var credentials = services[0].credentials;
+  ca = new Buffer(credentials.ca_certificate_base64, 'base64');
+  connectionString = credentials.uri;
+}
+
 var db = {};
-var sequelize = new Sequelize (config.database, config.username, config.password, config);
+
+if(env == 'test')
+{
+  sequelize = new Sequelize (config.database, config.username, config.password, config);
+}
+else
+{
+  sequelize = new Sequelize (connectionString, {
+    dialectOptions: { ssl: true},
+    dialect: 'postgres'
+  });
+}
 
 fs
   .readdirSync(__dirname)
